8792e24b069446eb58fa59e1785833ab8ab69767
Evgeni <Kremeni@gmail.com>
2022-08-16 14:24:51 +0300

    Добавлена домашняя работа 2 занятия

---
+++ HomeWork_2/src/main/java/ru/kremenia/AppConfiguration.java
@@ -0,0 +1,10 @@
+package ru.kremenia;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.kremenia")
+public class AppConfiguration {
+
+}
